93c2cc872faf25f08eac07bbc6f54183113aec83
hoangtien <htien2740@gmail.com>
2023-04-09 12:31:27 +0700

    y

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-let len = document.querySelector('.len');
-let xuong = document.querySelector('.xuong');
-let item1 = document.querySelectorAll('.item1');
-let item2 = document.querySelector('.item2');
-let item3 = document.querySelector('.item3');
-let clicklx = (id1, id2, id3)=>{
-    id1.style.transform="translateY(0%)";
-    
+// Lấy danh sách các slide
+const slides = document.querySelectorAll('.feedback_listItem');
+// Khai báo biến index hiện tại
+let currentIndex = 0;
+
+// Hàm ẩn slide hiện tại và hiển thị slide tiếp theo
+function showSlide(index) {
+  slides[currentIndex].classList.remove('active');
+  slides[index].classList.add('active');
+  currentIndex = index;
 }
-len.addEventListener('click',clicklx(item1,item2,item3))
+
+// Xử lý sự kiện click cho button chuyển slide trái
+document.querySelector('.len').addEventListener('click', function() {
+  const nextIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
+  showSlide(nextIndex);
+});
+
+// Xử lý sự kiện click cho button chuyển slide phải
+document.querySelector('.xuong').addEventListener('click', function() {
+  const nextIndex = currentIndex === slides.length - 1 ? 0 : currentIndex + 1;
+  showSlide(nextIndex);
+});
+
+// Hiển thị slide đầu tiên
+showSlide(currentIndex);
